9970ebb7b7794f79d754fc3ca2d9bfcf4b955ed2
GARCIAJXPSL702X\JohnPaul <garciaj3794@uhcl.edu>
2019-05-14 14:37:23 -0500

    added detail.html for job detail with back button.  Tested functionality of pages with admin content adds.

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.summary
--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job
 # Create your views here.
-def johnpaul(request):
+def home(request):
     jobs = Job.objects  # django pulls all jobs from the db.
     # pass the jobs to home.html with a dictionary of jobs
     return render(request, 'jobs/home.html', {'jobs': jobs})
+
+def detail(request, job_id):
+    job_detail = get_object_or_404(Job, pk=job_id)
+    return render(request, 'jobs/detail.html', {'job':job_detail})
