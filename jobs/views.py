00f1cedc93ee9a21745a48180acf4ccf3840dea1
GARCIAJXPSL702X\JohnPaul <garciaj3794@uhcl.edu>
2019-05-13 19:24:24 -0500

    added bootstrap css, js, jquery, and popper js.

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
 def johnpaul(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects  # django pulls all jobs from the db.
+    # pass the jobs to home.html with a dictionary of jobs
+    return render(request, 'jobs/home.html', {'jobs': jobs})
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import jobs.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.johnpaul, name='johnpaul'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
