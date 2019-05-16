bdc3c53e9d34c291047fa6ec84a59a1267c7c2f0
GARCIAJXPSL702X\JohnPaul <garciaj3794@uhcl.edu>
2019-05-16 12:30:47 -0500

    change background color and rearranged some text.

--- jobs/models.py
+++ jobs/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    githuburl = models.CharField(max_length=100)
 
     def __str__(self):
         return self.summary
